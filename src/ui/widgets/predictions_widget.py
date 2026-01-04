9fe1f481119e24c09437de1ed7af71656975dd54
Ana Miron <anamiron05@gmail.com>
2026-01-04 17:31:43 +0200

    letter transition noise filter and static/dynamic live prediction

--- src/ui/widgets/predictions_widget.py
+++ src/ui/widgets/predictions_widget.py
@@ -42,12 +42,14 @@ class PredictionsWidget(QWidget, Ui_widgetPredictions):
             self._last_gesture = ""
             return
 
+        current_gesture = "None"
         current_time = time.time()
         if current_time - self._last_prediction_time < 1.0 / self._MAX_PREDICTIONS_PER_SECOND:
             return
 
         gesture_id = self._predictor._classifier.calculate_movement_type()
-        current_gesture = str(gesture_id)
+        if current_gesture and current_gesture != gesture_id:
+            current_gesture = str(gesture_id)
 
         self.labelCurrentPrediction.setText(current_gesture)
 
