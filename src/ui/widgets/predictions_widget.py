5725a8c9d8118c2ca701eb9c2ddd7c1287c1a121
TrojiGareer <yanis.nicolau@gmail.com>
2026-01-05 19:48:54 +0200

    Added user manual

--- src/ui/widgets/predictions_widget.py
+++ src/ui/widgets/predictions_widget.py
@@ -1,14 +1,102 @@
 import time
 import traceback
 
-from PyQt6.QtWidgets import QWidget, QListWidgetItem
+# ADDED: QScrollArea
+from PyQt6.QtWidgets import (QWidget, QListWidgetItem, QDialog, QVBoxLayout, 
+                             QLabel, QPushButton, QTabWidget, QScrollArea)
 from PyQt6.QtCore import Qt
 
 from ml import Predictor
-
 from .auto_predictions_widget import Ui_widgetPredictions
-
 from utils import STATIC_MODEL_PATH, DYNAMIC_MODEL_PATH
+
+class HelpDialog(QDialog):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.setWindowTitle("M.I.R.A. Command Manual")
+        self.setFixedSize(400, 320) 
+
+        layout = QVBoxLayout()
+        self.tabs = QTabWidget()
+        
+        self.tab_activation = QWidget()
+        self.layout_act = QVBoxLayout()
+        lbl_act = QLabel("""
+            <h3 style="color:#a05ea0;">System Status</h3>
+            <p style="font-size:14px;">👍 <b>Thumbs Up:</b><br>ACTIVATE Mouse Control</p>
+            <p style="font-size:14px;">✊ <b>Fist:</b><br>DEACTIVATE Mouse</p>
+            <br>
+            <p style="color:gray; font-style:italic;">Use these to toggle M.I.R.A. on and off.</p>
+        """)
+        lbl_act.setWordWrap(True)
+        lbl_act.setAlignment(Qt.AlignmentFlag.AlignTop)
+        self.layout_act.addWidget(lbl_act)
+        self.tab_activation.setLayout(self.layout_act)
+
+        self.tab_mouse = QWidget()
+        self.layout_mouse = QVBoxLayout()
+        lbl_mouse = QLabel("""
+            <h3 style="color:#a05ea0;">Cursor Control</h3>
+            <p style="font-size:14px;">☝️ <b>Index Finger:</b><br>Move Cursor</p>
+            <p style="font-size:14px;">🤏 <b>Pinch Index + Thumb:</b><br>Left Click / Drag</p>
+            <p style="font-size:14px;">🖕 <b>Pinch Middle + Thumb:</b><br>Right Click</p>
+        """)
+        lbl_mouse.setWordWrap(True)
+        lbl_mouse.setAlignment(Qt.AlignmentFlag.AlignTop)
+        self.layout_mouse.addWidget(lbl_mouse)
+        self.tab_mouse.setLayout(self.layout_mouse)
+
+        self.tab_utils = QWidget()
+        self.layout_utils_outer = QVBoxLayout(self.tab_utils)
+        
+        scroll = QScrollArea()
+        scroll.setWidgetResizable(True)
+        scroll.setStyleSheet("QScrollArea { border: none; }")
+
+        scroll_content = QWidget()
+        self.layout_utils = QVBoxLayout(scroll_content)
+
+        lbl_utils = QLabel("""
+            <h3 style="color:#a05ea0;">Windows Shortcuts</h3>
+            <p style="font-size:14px;">🤘 <b>Ring Fold (Rock On):</b><br>Open Task View (Win + Tab)</p>
+            <p style="font-size:12px; color:gray;">(Keep Index, Middle, Pinky UP. Fold Ring finger)</p>
+            
+            <hr>
+            
+            <h3 style="color:#a05ea0;">Scrolling & Audio</h3>
+            <p style="font-size:14px;">✌️ <b>Victory Sign:</b> Enable Scroll</p>
+            <ul style="font-size:13px;">
+                <li>Hand High: Scroll UP</li>
+                <li>Hand Low: Scroll DOWN</li>
+            </ul>
+            <br>
+            <p style="font-size:14px;">👐 <b>Two Hands Open:</b> Volume Control</p>
+            <ul style="font-size:13px;">
+                <li>Move hands apart: Volume UP</li>
+                <li>Move hands together: Volume DOWN</li>
+            </ul>
+        """)
+        lbl_utils.setWordWrap(True)
+        lbl_utils.setAlignment(Qt.AlignmentFlag.AlignTop)
+        
+        self.layout_utils.addWidget(lbl_utils)
+        
+        scroll.setWidget(scroll_content)
+        
+        self.layout_utils_outer.addWidget(scroll)
+
+        self.tabs.addTab(self.tab_activation, "Activation")
+        self.tabs.addTab(self.tab_mouse, "Mouse")
+        self.tabs.addTab(self.tab_utils, "Utils")
+
+        layout.addWidget(self.tabs)
+
+        btn_close = QPushButton("Got it!")
+        btn_close.setCursor(Qt.CursorShape.PointingHandCursor)
+        btn_close.clicked.connect(self.accept)
+        layout.addWidget(btn_close)
+
+        self.setLayout(layout)
 
 class PredictionsWidget(QWidget, Ui_widgetPredictions):
     _MAX_PREDICTIONS_PER_SECOND = 2
@@ -23,10 +111,14 @@ class PredictionsWidget(QWidget, Ui_widgetPredictions):
         self._last_prediction_time = 0.0
         self._last_gesture = ""
 
+    def show_help_dialog(self):
+        """Opens the user manual popup. Called by Main Window."""
+        dialog = HelpDialog(self)
+        dialog.exec()
+
     def clear(self):
         self.listPredictionLog.clear()
         self.labelCurrentPrediction.setText("-")
-
         self._last_gesture = ""
         self._last_prediction_time = 0.0
     
@@ -58,7 +150,6 @@ class PredictionsWidget(QWidget, Ui_widgetPredictions):
         if current_gesture != self._last_gesture:
             item = QListWidgetItem(current_gesture)
             item.setTextAlignment(Qt.AlignmentFlag.AlignCenter)
-            # Insert at the top
             self.listPredictionLog.insertItem(0, item)
 
             if self.listPredictionLog.count() > self._MAX_PREDICTIONS_IN_LOG:
